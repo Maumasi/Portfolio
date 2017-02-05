7ead2c222983a1ad9136b0db3a02db923fb59b54
maumasi <liu.maumasi@gmail.com>
2017-02-05 15:23:35 -0500

    add uglify to the gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,27 @@
 
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
 
 // compile ES6 to legacy Javascript
 gulp.task('compileES6', () => {
-    return gulp.src(['./ES6/classes/es6/*.js'])
-      .pipe(babel({presets: ['es2015']}))
-      .pipe(gulp.dest('./ES6/classes/js'))
+  return gulp.src(['./ES6/classes/es6/*.js'])
+    .pipe(babel({presets: ['es2015']}))
+    .pipe(gulp.dest('./ES6/classes/js/compiledES6'))
+});
+
+// uglify the compiled ES6
+// This gives us a chance to make changes to the compiled ES6 if we need to
+gulp.task('compressJS', function () {
+  return gulp.src('./ES6/classes/js/compiledES6/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./ES6/classes/js'));
 });
 
 // watch for sass file changes on save
 gulp.task('watch', () => {
-    gulp.watch('ES6/classes/es6/*.js', ['compileES6']);
+  gulp.watch('ES6/classes/es6/*.js', ['compileES6', 'compressJS']);
 });
 
 // just type `gulp` in the terminal to execute all the gulp tasks
-gulp.task('default', ['compileES6', 'watch'])
+gulp.task('default', ['compileES6', 'compressJS', 'watch']);
