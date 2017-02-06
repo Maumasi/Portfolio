579a19edb8c6def8443859b671f8bbf90e270204
maumasi <liu.maumasi@gmail.com>
2017-02-06 01:07:34 -0500

    started SASS example

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+
 
 // compile ES6 to legacy Javascript
 gulp.task('compileES6', () => {
@@ -9,6 +11,7 @@ gulp.task('compileES6', () => {
     .pipe(babel({presets: ['es2015']}))
     .pipe(gulp.dest('./ES6/classes/es6/compiledES6'))
 });
+
 
 // uglify the compiled ES6
 // This gives us a chance to make changes to the compiled ES6 if we need to
@@ -18,11 +21,21 @@ gulp.task('compressJS', function () {
     .pipe(gulp.dest('./ES6/classes/js'));
 });
 
+
+// compile SASS files
+gulp.task('sass', () => {
+  return gulp.src('./SASS/sass/styles.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./SASS/css/'));
+});
+
+
 // watch for sass file changes on save
 // If there are any changes on save, run this array of gulp tasks
 gulp.task('watch', () => {
-  gulp.watch('ES6/classes/es6/*.js', ['compileES6', 'compressJS']);
+  gulp.watch(['ES6/classes/es6/*.js', 'SASS/sass/styles.sass'], ['compileES6', 'compressJS', 'sass']);
 });
 
+
 // just type `gulp` in the terminal to execute all the gulp tasks
-gulp.task('default', ['compileES6', 'compressJS', 'watch']);
+gulp.task('default', ['compileES6', 'compressJS', 'sass', 'watch']);
